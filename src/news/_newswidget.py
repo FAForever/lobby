c479a2d95748b89736aa249fb326f8b783240b30
gatsik <74517072+Gatsik@users.noreply.github.com>
2024-04-12 20:26:55 +0300

    Remove QtWebEngine usage from NewsWidget

--- src/news/_newswidget.py
+++ src/news/_newswidget.py
@@ -1,12 +1,18 @@
 import logging
-import webbrowser
 
-from PyQt6 import QtCore
 from PyQt6 import QtWidgets
+from PyQt6.QtCore import QByteArray
+from PyQt6.QtCore import QPoint
+from PyQt6.QtCore import QSize
+from PyQt6.QtCore import QUrl
+from PyQt6.QtGui import QImage
+from PyQt6.QtGui import QTextDocument
+from PyQt6.QtNetwork import QNetworkAccessManager
+from PyQt6.QtNetwork import QNetworkReply
+from PyQt6.QtNetwork import QNetworkRequest
 
 import util
 from config import Settings
-from util.qt import ExternalLinkPage
 
 from .newsitem import NewsItem
 from .newsitem import NewsItemDelegate
@@ -15,60 +21,33 @@ from .newsmanager import NewsManager
 logger = logging.getLogger(__name__)
 
 
-class Hider(QtCore.QObject):
-    """
-    Hides a widget by blocking its paint event. This is useful if a
-    widget is in a layout that you do not want to change when the
-    widget is hidden.
-    """
-
-    def __init__(self, parent=None):
-        super(Hider, self).__init__(parent)
-
-    def eventFilter(self, obj, ev):
-        return ev.type() == QtCore.QEvent.Type.Paint
-
-    def hide(self, widget):
-        widget.installEventFilter(self)
-        widget.update()
-
-    def unhide(self, widget):
-        widget.removeEventFilter(self)
-        widget.update()
-
-    def hideWidget(self, sender):
-        if sender.isWidgetType():
-            self.hide(sender)
-
-
 FormClass, BaseClass = util.THEME.loadUiType("news/news.ui")
 
 
 class NewsWidget(FormClass, BaseClass):
-    CSS = util.THEME.readstylesheet('news/news_webview.css')
+    CSS = util.THEME.readstylesheet('news/news_style.css')
 
-    HTML = str(util.THEME.readfile('news/news_webview_frame.html'))
+    HTML = util.THEME.readfile('news/news_page.html')
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         BaseClass.__init__(self, *args, **kwargs)
 
         self.setupUi(self)
 
+        self.nam = QNetworkAccessManager()
+        self.reply: QNetworkReply | None = None
+
         self.newsManager = NewsManager(self)
         self.newsItems = []
+        self.images = {}
 
         # open all links in external browser
-        self.newsWebView.setPage(ExternalLinkPage(self))
-
-        # hide webview until loaded to avoid FOUC
-        self.hider = Hider()
-        self.hider.hide(self.newsWebView)
-        self.newsWebView.loadFinished.connect(self.loadFinished)
+        self.newsTextBrowser.setOpenExternalLinks(True)
 
         self.settingsFrame.hide()
         self.hideNewsEdit.setText(Settings.get('news/hideWords', ""))
 
-        self.newsList.setIconSize(QtCore.QSize(0, 0))
+        self.newsList.setIconSize(QSize(0, 0))
         self.newsList.setItemDelegate(NewsItemDelegate(self))
         self.newsList.currentItemChanged.connect(self.itemChanged)
         self.newsSettings.pressed.connect(self.showSettings)
@@ -80,62 +59,74 @@ class NewsWidget(FormClass, BaseClass):
         newsItem = NewsItem(newsPost, self.newsList)
         self.newsItems.append(newsItem)
 
-    # QtWebEngine has no user CSS support yet, so let's just prepend it to the
-    # HTML
-    def _injectCSS(self, body, link, img):
-        img = (
-            '<div style="float:left;"><p style="float:left;"><img src={} '
-            'border="1px" hspace=20></p>'.format(img)
-        )
-        body = body + '</div>'
-        link = (
-            '<div style="clear:left;"><a href={} style="margin: 0px 0px '
-            '0px 20px">Open in your Web browser</a></div>'.format(link)
-        )
-        css = '<style type="text/css">{}</style>'.format(self.CSS)
-        return css + img + body + link
-
-    def updateNews(self):
-        self.hider.hide(self.newsWebView)
+    def updateNews(self) -> None:
+        self.hider.hide(self.newsTextBrowser)
         self.newsItems = []
         self.newsList.clear()
         self.newsManager.WpApi.download()
 
-    def itemChanged(self, current, previous):
-        if current is not None:
-            if current.newsPost['external_link'] == '':
-                link = current.newsPost['link']
-            else:
-                link = current.newsPost['external_link']
-            self.newsWebView.page().setHtml(
-                self.HTML.format(
-                    title=current.newsPost['title'],
-                    content=self._injectCSS(
-                        current.newsPost['excerpt'], link,
-                        current.newsPost['img_url'],
-                    ),
-                ),
-            )
+    def download_image(self, img_url: QUrl) -> None:
+        request = QNetworkRequest(img_url)
+        self.reply = self.nam.get(request)
+        self.reply.finished.connect(self.item_image_downloaded)
 
-    def linkClicked(self, url):
-        webbrowser.open(url.toString())
+    def add_image_resource(self, img_url: QUrl, image_data: QByteArray) -> None:
+        img = QImage()
+        img.loadFromData(image_data)
+        scaled = img.scaled(QSize(900, 500))
 
-    def loadFinished(self, ok):
-        self.hider.unhide(self.newsWebView)
-        self.newsWebView.loadFinished.disconnect(self.loadFinished)
+        self.images[img_url] = scaled
+        self.newsTextBrowser.document().addResource(
+            QTextDocument.ResourceType.ImageResource,
+            img_url,
+            scaled,
+        )
+
+    def item_image_downloaded(self) -> None:
+        if self.reply.error() is not self.reply.NetworkError.NoError:
+            return
+        self.add_image_resource(self.reply.request().url(), self.reply.readAll())
+        self.show_newspage()
+
+    def itemChanged(self, current: NewsItem | None, previous: NewsItem | None) -> None:
+        if current is None:
+            return
+        url = QUrl(current.newsPost["img_url"])
+        if url in self.images:
+            self.show_newspage()
+        else:
+            self.download_image(url)
+
+    def show_newspage(self) -> None:
+        current = self.newsList.currentItem()
+
+        if current.newsPost['external_link'] == '':
+            external_link = current.newsPost['link']
+        else:
+            external_link = current.newsPost['external_link']
+
+        content = current.newsPost["excerpt"].strip().removeprefix("<p>").removesuffix("</p>")
+        html = self.HTML.format(
+            style=self.CSS,
+            title=current.newsPost['title'],
+            content=content,
+            img_source=current.newsPost["img_url"],
+            external_link=external_link,
+        )
+        self.newsTextBrowser.setHtml(html)
 
     def showAll(self):
         for item in self.newsItems:
             item.setHidden(False)
         self.updateLabel(0)
 
-    def showEditToolTip(self):
+    def showEditToolTip(self) -> None:
         """
         Default tooltips are too slow and disappear when user starts typing
         """
         widget = self.hideNewsEdit
         position = widget.mapToGlobal(
-            QtCore.QPoint(0 + widget.width(), 0 - widget.height() / 2),
+            QPoint(0 + widget.width(), 0 - widget.height() / 2),
         )
         QtWidgets.QToolTip.showText(
             position,
